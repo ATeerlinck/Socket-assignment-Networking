62559982a95ed9760f7dfa56c873508469fd0a1d
Alex Teerlinck <ateerlinckwork@gmail.com>
2024-03-07 01:30:18 -0600

    some work on start of server side work

--- Assignment3.cs
+++ Assignment3.cs
@@ -7,12 +7,15 @@ using System.Text;
 // Note:	This program intentionally does not perform exception handling
 //			This will allow you to more easily diagnose issues with your connection and protocol
 
-public static class Assignment3 {
-	private static void Main() {
+public static class Assignment3
+{
+	private static void Main()
+	{
 		Client();
 	}
 
-	private static void Client() {
+	private static void Client()
+	{
 		// Create a TCP stream socket local to this computer using port 7777
 		var ipAddress = IPAddress.Parse("127.0.0.1");
 		var remoteEp = new IPEndPoint(ipAddress, 7777);
@@ -25,28 +28,33 @@ public static class Assignment3 {
 		var textReceived = Encoding.ASCII.GetString(buffer, 0, numBytesReceived);
 
 		// If the server does not respond with the 'hello' message, terminate the program
-		if (textReceived != "hello\r\n") {
+		if (textReceived != "hello\r\n")
+		{
 			Console.WriteLine("Server is not ready yet");
 			return;
 		}
 
 		// Loop forever to receive user input
-		for (; ; ) {
+		for (; ; )
+		{
 			// Allow the user to guess a number, or an empty string to quit
 			Console.Write("Enter your guess (leave empty to disconnect): ");
 			var input = Console.ReadLine();
 
 			string message;
-			if (string.IsNullOrWhiteSpace(input)) {
+			if (string.IsNullOrWhiteSpace(input))
+			{
 				// If the user's input was empty, our message to the server will be a 'quit' message
 				input = null;
 				message = "quit\r\n";
 			}
-			else if (int.TryParse(input, out int guess)) {
+			else if (int.TryParse(input, out int guess))
+			{
 				// If the user entered a valid integer, our message will be a 'guess' message
 				message = string.Format("guess\t{0}\r\n", input);
 			}
-			else {
+			else
+			{
 				// If the user entered anything else, make them to try again
 				Console.WriteLine("Invalid input");
 				continue;
@@ -57,7 +65,8 @@ public static class Assignment3 {
 			sender.Send(bytes);
 
 			// If the user didn't enter anything, the client can safely disconnect and end the program
-			if (input == null) {
+			if (input == null)
+			{
 				break;
 			}
 
@@ -66,12 +75,14 @@ public static class Assignment3 {
 			textReceived = Encoding.ASCII.GetString(buffer, 0, numBytesReceived);
 
 			// If the server responded with a 'correct' message, tell the user they won and stop the loop
-			if (textReceived == "correct\r\n") {
+			if (textReceived == "correct\r\n")
+			{
 				Console.WriteLine("Congratulations! You won!");
 				break;
 			}
 			// The server should have responded with either a 'too-high' or 'too-low' message as a hint
-			else {
+			else
+			{
 				Console.WriteLine("Incorrect guess. Here's a hint: {0}", textReceived.TrimEnd());
 			}
 		}
@@ -80,40 +91,52 @@ public static class Assignment3 {
 		sender.Shutdown(SocketShutdown.Both);
 	}
 
-	private static void Server() {
+	private static void Server()
+	{
 		throw new NotImplementedException("Create the server software by following these steps for Assignment 3");
 
 		// Create a TCP stream socket bound to 127.0.0.1 and port 7777 to listen for incoming clients
 		var ipAddress = IPAddress.Parse("127.0.0.1");
-		var remoteEp = new IPEndPoint(ipAddress, 7777);
+		var localEp = new IPEndPoint(ipAddress, 7777);
 
-		using var listener = new Socket(ipAddress.AddressFamily,
-SocketType.Stream, ProtocolType.Tcp);
-		
-		
+		using var listener = new Socket(ipAddress.AddressFamily,SocketType.Stream, ProtocolType.Tcp);
+		listener.Bind(localEp);
+
+		var buffer = new byte[1024];
+
 		// Loop forever to listen for connections
-		for (; ;)
-			{
+		for (; ; )
+		{
+			Console.WriteLine("Waiting...");
+			// Accept an incoming connection, creating a socket for that client
+			using var handler = listener.Accept();
+			var numBytesReceived = handler.Receive(buffer);
+			// Generate a random integer from 1 to 99 (inclusive)
+			var rand = new Random().Next(100);
+		// Send a 'hello' message to the client
+			Console.WriteLine("Hello!");
+		// Loop forever to receive new messages from this client
+			for(; ; ){
+				Console.WriteLine("Waiting for message...");
+		// 		Receive an incoming message and decode it using ASCII encoding
+				var textReceived = Encoding.ASCII.GetString(buffer, 0, numBytesReceived);
+
 				
 			}
-		// Accept an incoming connection, creating a socket for that client
 
-		// Generate a random integer from 1 to 99 (inclusive)
 
-		// Send a 'hello' message to the client
 
-		// Loop forever to receive new messages from this client
 
-		// 		Receive an incoming message and decode it using ASCII encoding
 
 		// 		If the message is a 'quit' message, disconnect from this client and start listening again
 
 		// 		If the message is a 'guess' message, parse the client's guess, then...
 
 		// 			...If their guess matches the answer, send a 'correct' message to them and disconnect from the client
-		
+
 		// 			...If their guess is greater than the answer, send a 'too-high' message and continue listening
 
 		// 			...If their guess is less than the answer, send a 'too-low' message and continue listening
+		}
 	}
 }
